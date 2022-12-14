46544121b4beb32eafcba5c4ea9bb4844146f9c9
mba-tsk <113594342+mba-tsk@users.noreply.github.com>
2022-12-14 20:47:41 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -163,7 +163,7 @@ var html = `<!DOCTYPE html>
         margin: 0;
         width: 100%%;
         height: 100%%;
-        background: red;
+        background: green;
     }
     #log {
         background: white;
